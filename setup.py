562b2017389b8b47349bd6c232370d6371612c3a
Scott <scotts@nsidc.org>
2019-09-03 12:44:36 -0600

    added minimal code to setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='geogrid',
+    version='0.1.0',
+    description='classes for provideing GeoGrid and GeoPoint',
+    packages=find_packages(exclude='contrib', 'docs', 'tests']),
+    python_requires='>=3.6',
+)
+
