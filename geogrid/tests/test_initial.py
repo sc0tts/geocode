23330126062b9aecfa955c383856514f321bcae7
Scott <scotts@nsidc.org>
2019-09-04 10:43:33 -0600

    add initial test file, testing imports

---
+++ geogrid/tests/test_initial.py
@@ -0,0 +1,15 @@
+"""
+test_initial.py
+
+Initial tests to ensure project layout is adequate
+"""
+
+import numpy as np
+from osgeo import gdal
+import geogrid
+
+def test_imports():
+    """Test that standard imports raise no exceptions"""
+    pass
+
+
